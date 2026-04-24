371f7f4cc2ae476021e14e67b87635bb230968d0
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-24 12:54:02 +0530

    LoginPage

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,5 +1,9 @@
 import { expect, type Page } from '../fixtures/base.js'
 
+const USERNAME_LOCATOR = "xpath=//input[@name='username']"
+const PASSWORD_LOCATOR = "xpath=//input[@name='password']"
+const LOGIN_LOCATOR = "xpath=//button[contains(normalize-space(),'Login')]"
+const ERROR_LOCATOR = "xpath=//p[text()='Invalid credentials']"
 
 class LoginPage {
     private readonly page: any;
@@ -9,18 +13,20 @@ class LoginPage {
     }
 
     async fillUsername(username: string): Promise<void> {
-        await this.page.locator("xpath=//input[@name='username']").fill(username)
+        await this.page.locator(USERNAME_LOCATOR).fill(username)
     }
     async fillPassword(password: string): Promise<void> {
-        await this.page.locator("xpath=//input[@name='password']").fill(password)
+        await this.page.locator(PASSWORD_LOCATOR).fill(password)
     }
     async clickLogin(): Promise<void> {
-        await this.page.locator("xpath=//input[@name='password']").click()
+        await this.page.locator(LOGIN_LOCATOR).click()
     }
-    async verifyInvalidErrorMessage(expected_error: string) {
-        await expect(this.page.locator("xpath=//p[text()='Invalid credentials']")).toHaveText(expected_error)
+    async verifyInvalidErrorMessage(expected_error: string): Promise<void> {
+        await expect(this.page.locator(ERROR_LOCATOR)).toHaveText(expected_error)
     }
-
+    async verifyUserNamePlaceholder(): Promise<void> {
+        await expect.soft(this.page.locator(USERNAME_LOCATOR)).toHaveAttribute('placeholder', 'Username')
+    }
 }
 
 export { LoginPage }
