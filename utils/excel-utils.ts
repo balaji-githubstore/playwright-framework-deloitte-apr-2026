f6559933549e48f7c59020f350925131b2fe0fad
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-24 12:17:50 +0530

    Updated the excel data driven

--- utils/excel-utils.ts
+++ utils/excel-utils.ts
@@ -5,16 +5,13 @@ const EXCEL_FILE = 'test-data/hrm-data.xlsx'
 class ExcelUtils {
 
     static getSheetToJson(sheetname: string): any {
-
         const workbook = XLSX.readFile(EXCEL_FILE)
         const sheet = workbook.Sheets[sheetname]
-
         if (!sheet) {
             throw new Error(`Sheet ${sheetname} not found`)
         }
         const data = XLSX.utils.sheet_to_json(sheet)
         return data;
-
     }
 }
 
