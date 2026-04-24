f2b97330e92f12c8558d6ad0d3b68c3986227c5c
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-24 12:58:03 +0530

    Dashboard Page

--- pages/DashboardPage.ts
+++ pages/DashboardPage.ts
@@ -1,9 +1,15 @@
 import { expect, type Page } from '../fixtures/base.js'
 
+const DASHBOARD_LOCATOR = "xpath=//h6[text()='Dashboard']"
+
 class Dashboard {
     private readonly page: Page;
 
     constructor(page: Page) {
         this.page = page;
+    }
+
+    async verifyDashboardHeader(expected_error: string): Promise<void> {
+        await expect(this.page.locator(DASHBOARD_LOCATOR)).toHaveText(expected_error)
     }
 }
