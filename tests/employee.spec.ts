c18a0241515de62a27700236e2af8781ad78626b
balaji-githubstore <balajidinakaran1@gmail.com>
2026-04-24 14:41:59 +0530

    Add employee data for testing and refactor employee management tests to use dynamic data

--- tests/employee.spec.ts
+++ tests/employee.spec.ts
@@ -1,32 +1,42 @@
 import { test, expect } from '../fixtures/base.js';
 import { LoginPage } from '../pages/LoginPage.js';
 import { MainPage } from '../pages/MainPage.js';
+import { JsonUtils } from '../utils/json-utlis.js';
 
 test.describe("Employee management tests", () => {
-  test('verify adding new employee', async ({ page }) => {
-     const loginPage = new LoginPage(page);
-     await loginPage.fillUsername("Admin");
-     await loginPage.fillPassword("admin123");
-     await loginPage.clickLogin();
-     
-    //  MainPage
-    const mainPage=new MainPage(page);
-    //click on PiM menu
-    await mainPage.clickOnPIMMenu();
 
-    //PIMPage
-    //click on Add Employee
+  for (const { username, password, firstName, middleName, lastName, expectedProfileName, expectedFirstName } of JsonUtils.getJson('addEmployeeData')) {
 
-    //AddEmployeePage
-    //enter firstname as john
-    //enter middle name as w
-    //enter lastname as wick
-    //click on save
+    test(`verify valid login: ${firstName} and ${lastName}`, async ({ page }) => {
+      test('verify adding new employee', async ({ page }) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.fillUsername(username);
+        await loginPage.fillPassword(password);
+        await loginPage.clickLogin();
 
-    //PersonDetailPage 
-    //validate profile name - john wick
-    //validate firstname in the textbox 
-    //await expect.soft(page.locator("xpath=//input[@name='username']")).toHaveAttribute('value', 'john')
-   
-  });
+        //  MainPage
+        const mainPage = new MainPage(page);
+        //click on PiM menu
+        await mainPage.clickOnPIMMenu();
+
+        //PIMPage
+        //click on Add Employee
+
+        //AddEmployeePage
+        //enter firstname as john
+        //enter middle name as w
+        //enter lastname as wick
+        //click on save
+
+        //PersonDetailPage 
+        //validate profile name - john wick
+        //validate firstname in the textbox 
+        //await expect.soft(page.locator("xpath=//input[@name='username']")).toHaveAttribute('value', 'john')
+
+      });
+    });
+  }
+
+
+
 })
